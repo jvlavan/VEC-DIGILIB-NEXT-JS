edc27aafbe502e433f72d1e9b1294ab47f6bf003
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2024-05-31 15:43:46 +0530

    account deletion redirect bug fixed

--- app/delete/page.jsx
+++ app/delete/page.jsx
@@ -4,8 +4,47 @@ import { Toaster, toast } from "react-hot-toast";
 import { Spinner } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { SignIn, useUser, RedirectToSignIn } from "@clerk/nextjs";
 
 function App() {
+  const { user } = useUser();
+  const[issignin,setissignin]=useState(false)
+  if (!user) {
+    return (
+      <section className="min-w-screen grid  gap-4 mt-40 bg-black place-items-center">
+        {issignin && <RedirectToSignIn />
+        }
+        <div className="p-4 md:p-5 text-center">
+          <Toaster />
+          <svg
+            className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+            Please sign in to delete your account
+          </h3>
+          <button
+            onClick={()=>setissignin(true)}
+            type="button"
+            className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+          >
+            Sign me in to delete my account
+          </button>
+        </div>
+      </section>
+    );
+  }
   return (
     <section className="min-w-screen grid  gap-4 mt-40 bg-black place-items-center">
       <div className="p-4 md:p-5 text-center">
