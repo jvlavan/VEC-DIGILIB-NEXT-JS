60b49abcfced6a0dc8c1262f9fc883c07b19642a
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2024-05-31 15:30:06 +0530

    404 fixed

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in",
   "/sign-up",
   "/api/llmgen",
-  "/deleted"
+  "/deleted",
+  "/delete",
 ]);
 
 export default clerkMiddleware((auth, request) => {
