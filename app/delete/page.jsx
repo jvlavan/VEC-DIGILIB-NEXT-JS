2dd0f31e64e188371f2ac99704ca3a2cf3a649e5
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2024-05-31 15:04:52 +0530

    Account deletion implemented

--- app/delete/page.jsx
+++ app/delete/page.jsx
@@ -19,9 +19,9 @@ function App() {
         >
           <path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
           />
         </svg>
@@ -44,7 +44,7 @@ function App() {
           type="button"
           className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
         >
-          Yes, I'm sure
+          Yes, I&apos;m sure
         </button>
       </div>
     </section>
